d0452ee897bf27f166ec015bd0fe861b56c5995a
Nikifor Dragov <ndragov99@gmail.com>
2023-05-27 14:39:24 +0300

    Adding views and static files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
