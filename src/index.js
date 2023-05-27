d44be474f470cab6f62fe3b085c75efacb3611dc
Nikifor Dragov <ndragov99@gmail.com>
2023-05-27 16:19:43 +0300

    Add create route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 const app = express();
 
@@ -11,6 +12,7 @@ const PORT = 5000;
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(homeController)
+app.use(homeController);
+app.use('/cubes', cubeController);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
