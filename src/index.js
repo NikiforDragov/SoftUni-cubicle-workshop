8204cb1b24331ba61a7bfe564ac0afdf5c91ec6a
Nikifor Dragov <ndragov99@gmail.com>
2023-05-27 14:49:50 +0300

    Setting main view

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 const PORT = 5000;
 
+//handlebars config
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}))
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+//Routes
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.render('index');
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
