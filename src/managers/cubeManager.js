431127fdd49125388f48408266cf956d536c63c9
Nikifor Dragov <ndragov99@gmail.com>
2023-05-31 20:26:10 +0300

    Render cubes from db

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,25 +1,8 @@
 const Cube = require('../models/Cube');
 const uniqid = require('uniqid');
 
-const cubes = [
-    {
-        id: uniqid(),
-        name: 'cool',
-        description: 'cube',
-        imageUrl: 'https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg',
-        difficultyLevel: 2,
-    },
-    {
-        id: uniqid(),
-        name: 'hot',
-        description: 'box',
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61HpQqVQ37L._SY355_.jpg',
-        difficultyLevel: 4,
-    }
-];
-
-exports.getAll = (search, from, to) => {
-    let result = cubes.slice();
+exports.getAll = async (search, from, to) => {
+    let result = await Cube.find().lean()
 
     if (search) {
         result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
@@ -35,7 +18,7 @@ exports.getAll = (search, from, to) => {
 
     return result;
 }
-exports.getOne = (cubeId) => cubes.find(x => x.id === cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId)
 
 exports.create = async (cubeData) => {
     const cube = new Cube(cubeData);
