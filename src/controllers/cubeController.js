c198690f5fd447dc4d0af45123ce967197dc7f35
Nikifor Dragov <ndragov99@gmail.com>
2023-05-31 21:43:44 +0300

    Add attach-accessory lin

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -35,7 +35,7 @@ router.get('/:cubeId/details', async (req, res) => {
 })
 
 router.get('/:cubeId/attach-accessory', (req, res) => {
-    res.render('/accessory/attach');
+    res.render('accessory/attach');
 })
 
 module.exports = router;
