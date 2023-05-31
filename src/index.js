d2ad56c216bd0264ae9051d3d5a450ab59303a0c
Nikifor Dragov <ndragov99@gmail.com>
2023-05-31 19:41:53 +0300

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ const express = require('express');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const routes = require('./routes');
+const dbConnect = require('./config/dbConfig');
 
 const app = express();
 
 const PORT = 5000;
 
+dbConnect()
+    .then(() => console.log('Db connected successfully!'))
+    .catch(err => {
+        console.log('Db error: ', err);
+    });
+    
 expressConfig(app);
 handlebarsConfig(app);
 
