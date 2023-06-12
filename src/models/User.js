72e31f0719d3682cf6ebcbf5b84ebc20b972c8a1
Nikifor Dragov <ndragov99@gmail.com>
2023-06-12 22:04:03 +0300

    add unique usernames

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: {
+        type:String,
+        unique: true,
+    },
     password: {
         type: String,
         // validate: {
