41512b54daca41743d083b63cb4f0af0a201e060
Nikifor Dragov <ndragov99@gmail.com>
2023-06-12 17:12:50 +0300

    Add login functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,8 +14,16 @@ router.post('/register', async (req, res) => {
     res.redirect('/users/login');
 });
 
-router.get('/login', (req,res) => {
+router.get('/login', (req, res) => {
     res.render('users/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await userManager.login(username, password);
+
+    res.redirect('/');
 })
 
 module.exports = router;
