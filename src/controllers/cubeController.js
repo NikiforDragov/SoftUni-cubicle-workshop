bb399a4ef04e48c2f4095d3c236fb32ac542c9ec
Nikifor Dragov <ndragov99@gmail.com>
2023-06-12 19:45:00 +0300

    Add edit page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -64,5 +64,10 @@ router.post('/:cubeId/delete', async (req, res) => {
     res.redirect('/');
 })
 
+router.get('/:cubeId/edit', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    res.render('cube/edit', { cube })
+})
+
 
 module.exports = router;
